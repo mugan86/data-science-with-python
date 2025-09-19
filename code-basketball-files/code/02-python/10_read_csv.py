043bb72b9b035fdc5573164768dc91beeed73b2e
Anartz Mugika Ledo <mugan86@gmail.com>
2025-09-19 20:22:59 +0200

    Update 10_read_csv.py

--- code-basketball-files/code/02-python/10_read_csv.py
+++ code-basketball-files/code/02-python/10_read_csv.py
@@ -1,33 +1,13 @@
-
-
-# normally imports like this would be at the top of the file
-import os
-
-os.cpu_count()
-
-
-import pandas as pd
-from os import path
-
-print('hello')
-
-print(pd.__version__)
-
 ## Debe de ser la raíz donde está el directorio donde no encontremos, en este caso fuera de "code"
 PROJECT_PATH = "./code-basketball-files/"
 DATA_DIR = "data/"
 
-# normally imports like this would be at the top of the file
-import os
-
-os.cpu_count()
-
 from os import path
 
-ruta_fichero = path.join(PROJECT_PATH, DATA_DIR, "shot.csv")
-print("Buscando en:", ruta_fichero)
+filePath = path.join(PROJECT_PATH, DATA_DIR, "shot.csv")
+print("Buscando en:", filePath)
 
-with open(ruta_fichero, "r", encoding="utf-8") as f:
-    contenido = f.read()
-print(contenido)
+with open(filePath, "r", encoding="utf-8") as f:
+    contentData = f.read()
+print(contentData)
 
