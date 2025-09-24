4a94c3572742ad760c8fc35bfb783084dded7f28
Anartz Mugika Ledo <mugan86@gmail.com>
2025-09-24 19:55:28 +0200

    feat: working until 107 line

--- code-basketball-files/code/03-pandas/00_basics.py
+++ code-basketball-files/code/03-pandas/00_basics.py
@@ -5,12 +5,14 @@ from project.config import get_file_path, FileExtension, Project
 # Ejecutamos con 
 # python3 -m code-basketball-files.code.03-pandas.00_basics
 
-filePath = get_file_path("shot", FileExtension.CSV, Project.BASKETBALL)
-print("Buscando en:", filePath)
+filePath = get_file_path("shots", FileExtension.CSV, Project.BASKETBALL)
+print("Find Path:", filePath)
 
 ##############
 # Loading data
 ##############
+
+print("========== LOADING DATA AND TAKE TYPE ==========")
 shots = pd.read_csv(filePath)
 
 print(type(shots))
@@ -18,6 +20,8 @@ print(type(shots))
 ##################################
 # DataFrame methods and attributes
 ##################################
+
+print("========== DATAFRAME METHODS AND ATTRIBUTES ==========")
 print(shots.head())
 
 print(shots.columns)
@@ -27,15 +31,18 @@ print(f'Number rows and columns: {shots.shape}')
 #################################
 # Working with subsets of columns
 #################################
+print("========== SUBSETS OF COLUMNS ==========")
 # A single column (5 items for default from start)
+print("----One Column----")
 print(shots['name'].head())
 
-type(shots['name'])
+print(type(shots['name']))
 
-shots['name'].to_frame().head()
-type(shots['name'].to_frame().head())
+print(shots['name'].to_frame().head())
+print(type(shots['name'].to_frame().head()))
 
 # Multiple columns
+print("----Multiple Column----")
 shots[['name', 'dist', 'value', 'made']].head()
 
 type(shots[['name', 'dist', 'value', 'made']].head())
@@ -46,32 +53,56 @@ type(shots[['name', 'dist', 'value', 'made']].head())
 ##########
 # Indexing
 ##########
-shots[['name', 'dist', 'value', 'made']].head()
+print("========== INDEXING ==========")
 
-shots.set_index('shot_id').head()
+print(shots[['name', 'dist', 'value', 'made']].head(3))
+
+print("--- Asigna como ID 'shot_id' ---")
+print("Todas las columnas")
+print(shots.set_index('shot_id').head()) # Muestra como Index el shot_id
+print("Columnas", 'name', 'dist', 'value', 'made')
+print(shots.set_index('shot_id')[['name', 'dist', 'value', 'made']].head()) # Muestra como Index el shot_id
 
 # Copies and the inplace argument
-shots.head()  # note: player_id not the index, even though we just set it
+## Muestra los 5 primeros elementos
+print("---Usando inplace---")
+print('Sin cambiarlo, tenemos el index normal')
+print(shots.head())  # note: id not the index, even though we just set it
 
-shots.set_index('shot_id', inplace=True)
-shots.head()  # now player_id is index
+shots.set_index('shot_id', inplace=True) # inplace fija como id principal shot_id
+print('Asignando "shot_id" como index')
 
-# alternate to using inplace, reassign adp
+print(shots.head())  # now player_id is index
+
+# En vez de usar Inplace, ahora recargamos y volvemos al index normal
 # reload shots with default 0, 1, ... index
 shots = pd.read_csv(get_file_path("shots", FileExtension.CSV, Project.BASKETBALL))
 shots = shots.set_index('shot_id')
 shots.head()  # now shot_id is index
 
-shots.reset_index().head()
+## Resetea el index shot_id por el index que viene por defecto con 0,1,2,...
+print(shots.reset_index().head())
+
 
 #############################
 # Indexes keep things aligned
 #############################
+## Lanzamientos en prorroga
+"""
+#### 🔹 **Filtrar con `.loc[]`**
+
+El método `.loc[]` permite acceder a filas basadas en condiciones:
+
+```python
+df_filtered = df.loc[df["Age"] > 28, ["Name", "Salary"]]
+print(df_filtered)
+```
+"""
 shots_ot = shots.loc[shots['period'] > 4, ['name', 'dist', 'value']]
-shots_ot.head()
+print(shots_ot.head())
 
 shots_ot.sort_values('name', inplace=True)
-shots_ot.head()
+print(shots_ot.head())
 
 # assigning a new column
 shots_ot['made'] = shots['made']
